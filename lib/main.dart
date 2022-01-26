b915b0dd7c89cdb49942010bf2d2aebc028d198e
Dion Laranjeira <dionribeiro.rr@gmail.com>
2022-01-26 08:53:52 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:app_aprenda_ingles/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
